e71e97e4f01f966b0bf085306fdbf17204dde854
Юлиана Стрельникова <julianka.str@gmail.com>
2023-07-06 19:29:03 +0300

    fix debug

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', '1234')
 
-DEBUG = bool(os.getenv('DEBUG', 'True'))
+DEBUG = os.getenv('DEBUG', 'False')
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
